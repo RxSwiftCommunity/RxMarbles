2ad02f9aa78693985140348c16b7fdcfe13b0697
yury <yury.korolev@gmail.com>
2016-01-23 23:57:57 +0300

    Peek and pop gesture support

--- RxMarbles/OperatorsTableViewController.swift
+++ RxMarbles/OperatorsTableViewController.swift
@@ -74,6 +74,19 @@ class OperatorsTableViewController: UITableViewController, UISearchResultsUpdati
             .map(_rowAtIndexPath)
             .subscribeNext { op in self.selectedOperator = op }
             .addDisposableTo(_disposeBag)
+        
+        // Check for force touch feature, and add force touch/previewing capability.
+        if traitCollection.forceTouchCapability == .Available {
+            /*  
+                Register for `UIViewControllerPreviewingDelegate` to enable
+                "Peek" and "Pop".
+                (see: MasterViewController+UIViewControllerPreviewing.swift)
+
+                The view controller will be automatically unregistered when it is
+                deallocated.
+            */
+            registerForPreviewingWithDelegate(self, sourceView: view)
+        }
     }
 
     // MARK: - Table view data source
@@ -137,4 +150,34 @@ class OperatorsTableViewController: UITableViewController, UISearchResultsUpdati
         tableView.reloadData()
     }
     
+}
+
+extension OperatorsTableViewController: UIViewControllerPreviewingDelegate {
+    /// Create a previewing view controller to be shown at "Peek".
+    func previewingContext(previewingContext: UIViewControllerPreviewing, viewControllerForLocation location: CGPoint) -> UIViewController? {
+        // Obtain the index path and the cell that was pressed.
+        guard let indexPath = tableView.indexPathForRowAtPoint(location),
+                  cell = tableView.cellForRowAtIndexPath(indexPath) else { return nil }
+        
+        // Create a detail view controller and set its properties.
+        let detailController = ViewController()
+        detailController.currentOperator = _rowAtIndexPath(indexPath)
+        
+        /*
+            Set the height of the preview by setting the preferred content size of the detail view controller.
+            Width should be zero, because it's not used in portrait.
+        */
+//        detailViewController.preferredContentSize = CGSize(width: 0.0, height: detailController.preferredHeight)
+        
+        // Set the source rect to the cell frame, so surrounding elements are blurred.
+        previewingContext.sourceRect = cell.frame
+        
+        return detailController
+    }
+    
+    /// Present the view controller for the "Pop" action.
+    func previewingContext(previewingContext: UIViewControllerPreviewing, commitViewController viewControllerToCommit: UIViewController) {
+        // Reuse the "Peek" view controller for presentation.
+        showDetailViewController(viewControllerToCommit, sender: self)
+    }
 }
