b0141893c5fdf96297726a63df6f172fa816452e
tutubalinry <tutubalinry@gmail.com>
2016-02-09 11:10:49 +0300

    Trash view position by Device screen size

--- RxMarbles/SceneView.swift
+++ RxMarbles/SceneView.swift
@@ -122,8 +122,8 @@ class SceneView: UIView, UITextViewDelegate {
         
         let size = rxOperatorText.text.boundingRectWithSize(CGSizeMake(bounds.width, 1000), options: NSStringDrawingOptions.UsesLineFragmentOrigin, attributes: [NSFontAttributeName: rxOperatorText.font!], context: nil).size
         rxOperatorText.frame = CGRectMake(0, resultTimeline.frame.origin.y + resultTimeline.frame.height + 10, bounds.width, size.height + 40)
-
-        if UIApplication.sharedApplication().statusBarOrientation.isLandscape && Device.isSmallerThanScreenSize(.Screen4_7Inch) {
+        
+        if UIApplication.sharedApplication().statusBarOrientation.isLandscape && Device.isSmallerThanScreenSize(.Screen4_7Inch) && Device.size() != .UnknownSize {
             trashView.center = CGPointMake(rxOperatorText.center.x, UIScreen.mainScreen().bounds.height - 60)
         } else {
             trashView.center = rxOperatorText.center
