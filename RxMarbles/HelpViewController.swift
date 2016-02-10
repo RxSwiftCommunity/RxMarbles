8ced6ab26c66a07b0c71a559fe179a021b190113
tutubalinry <tutubalinry@gmail.com>
2016-02-10 23:10:25 +0300

    HelpViewController configure

--- RxMarbles/HelpViewController.swift
+++ RxMarbles/HelpViewController.swift
@@ -16,7 +16,7 @@ class HelpViewController: AnimatedPagingScrollViewController {
     private let firstEventView = EventView(recorded: RecordedType(time: 0, event: .Next(ColoredType(value: "", color: Color.nextGreen, shape: EventShape.Circle))))
     private let secondEventView = EventView(recorded: RecordedType(time: 0, event: .Next(ColoredType(value: "", color: Color.nextBlue, shape: EventShape.Circle))))
     private let completedEventView = EventView(recorded: RecordedType(time: 0, event: .Completed))
-    private let actionButton = UIButton(type: .Custom)
+    private let nextButton = UIButton(type: .Custom)
     private let completedButton = UIButton(type: .Custom)
     private let firstHelpImage = UIImageView()
     private let secondHelpImage = UIImageView()
@@ -25,42 +25,48 @@ class HelpViewController: AnimatedPagingScrollViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .whiteColor()
-    
-//        configureScrollView()
+        
         configureLogoImageView()
-        configureActionButton()
+        configureImageViews()
+        configureNextButton()
         configureCompletedButton()
         configureResultTimeline()
-        configureHelpImageViews()
-        configureEventViews()
-    }
-    
-    private func configureScrollView() {
-        let backgroundColorAnimation = BackgroundColorAnimation(view: scrollView)
-        backgroundColorAnimation[0] = .whiteColor()
-        backgroundColorAnimation[2] = .whiteColor()
-        backgroundColorAnimation[3] = UIColor(red: 0.14, green: 0.8, blue: 1, alpha: 1)
-        animator.addAnimation(backgroundColorAnimation)
     }
     
     private func configureLogoImageView() {
-        logoImageView.backgroundColor = .lightGrayColor()
         contentView.addSubview(logoImageView)
-        contentView.addConstraint(NSLayoutConstraint(item: logoImageView, attribute: .Top, relatedBy: .Equal, toItem: contentView, attribute: .Top, multiplier: 1, constant: 50))
-        scrollView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:[logo(==80)]", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["logo" : logoImageView]))
-        scrollView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[logo(==80)]", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["logo" : logoImageView]))
+        let verticalConstraint = NSLayoutConstraint(item: logoImageView, attribute: .Top, relatedBy: .Equal, toItem: contentView, attribute: .Top, multiplier: 1, constant: -100)
+        contentView.addConstraint(verticalConstraint)
         keepView(logoImageView, onPages: [0, 1, 2, 3, 4])
+        
+        let verticalAnimation = ConstraintConstantAnimation(superview: contentView, constraint: verticalConstraint)
+        verticalAnimation[0] = 30
+        animator.addAnimation(verticalAnimation)
     }
     
-    private func configureActionButton() {
-        actionButton.setTitle("onNext()", forState: .Normal)
-        actionButton.setTitleColor(.blackColor(), forState: .Normal)
-        contentView.addSubview(actionButton)
-        contentView.addConstraint(NSLayoutConstraint(item: actionButton, attribute: .Bottom, relatedBy: .Equal, toItem: contentView, attribute: .Bottom, multiplier: 1, constant: 0))
-        keepView(actionButton, onPage: 0)
+    private func configureImageViews() {
+        configureImageViewsConstraints(firstHelpImage, page: 0)
         
-        let hideAnimation = HideAnimation(view: actionButton, hideAt: 0.9)
-        animator.addAnimation(hideAnimation)
+        configureImageViewsConstraints(secondHelpImage, page: 1)
+        
+        configureImageViewsConstraints(thirdHelpImage, page: 2)
+    }
+    
+    private func configureImageViewsConstraints(imageView: UIImageView, page: CGFloat) {
+        imageView.backgroundColor = .lightGrayColor()
+        contentView.addSubview(imageView)
+        contentView.addConstraint(NSLayoutConstraint(item: imageView, attribute: .CenterY, relatedBy: .Equal, toItem: contentView, attribute: .CenterY, multiplier: 1, constant: 0))
+        imageView.addConstraint(NSLayoutConstraint(item: imageView, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 100))
+        imageView.addConstraint(NSLayoutConstraint(item: imageView, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 100))
+        keepView(imageView, onPage: page)
+    }
+    
+    private func configureNextButton() {
+        nextButton.setTitle("onNext()", forState: .Normal)
+        nextButton.setTitleColor(.blackColor(), forState: .Normal)
+        contentView.addSubview(nextButton)
+        contentView.addConstraint(NSLayoutConstraint(item: nextButton, attribute: .Bottom, relatedBy: .Equal, toItem: contentView, attribute: .Bottom, multiplier: 1, constant: 0))
+        keepView(nextButton, onPage: 0)
     }
     
     private func configureCompletedButton() {
@@ -69,93 +75,26 @@ class HelpViewController: AnimatedPagingScrollViewController {
         contentView.addSubview(completedButton)
         contentView.addConstraint(NSLayoutConstraint(item: completedButton, attribute: .Bottom, relatedBy: .Equal, toItem: contentView, attribute: .Bottom, multiplier: 1, constant: 0))
         keepView(completedButton, onPage: 1)
-        
-        let hideAnimation = HideAnimation(view: completedButton, hideAt: 1.9)
-        animator.addAnimation(hideAnimation)
     }
     
     private func configureResultTimeline() {
         contentView.addSubview(resultTimeline)
-        contentView.addConstraint(NSLayoutConstraint(item: resultTimeline, attribute: .Bottom, relatedBy: .Equal, toItem: actionButton, attribute: .Top, multiplier: 1, constant: -30))
+        contentView.addConstraint(NSLayoutConstraint(item: resultTimeline, attribute: .Bottom, relatedBy: .Equal, toItem: nextButton, attribute: .Top, multiplier: 1, constant: 0))
         scrollView.addConstraint(NSLayoutConstraint(item: resultTimeline, attribute: .Width, relatedBy: .Equal, toItem: scrollView, attribute: .Width, multiplier: 0.9, constant: 0))
         keepView(resultTimeline, onPages: [0, 1, 2])
     }
     
-    private func configureHelpImageViews() {
-        let size = (view.frame.width > view.frame.height ? view.frame.height : view.frame.width) * 0.8
-        let metrics = ["size" : size]
-        
-        firstHelpImage.backgroundColor = .lightGrayColor()
-        contentView.addSubview(firstHelpImage)
-        contentView.addConstraint(NSLayoutConstraint(item: firstHelpImage, attribute: .CenterY, relatedBy: .Equal, toItem: contentView, attribute: .CenterY, multiplier: 1, constant: 0))
-        scrollView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:[image(==size)]", options: NSLayoutFormatOptions(rawValue: 0), metrics: metrics, views: ["image" : firstHelpImage]))
-        scrollView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[image(==size)]", options: NSLayoutFormatOptions(rawValue: 0), metrics: metrics, views: ["image" : firstHelpImage]))
-        keepView(firstHelpImage, onPage: 0)
-        
-        secondHelpImage.backgroundColor = .lightGrayColor()
-        contentView.addSubview(secondHelpImage)
-        contentView.addConstraint(NSLayoutConstraint(item: secondHelpImage, attribute: .CenterY, relatedBy: .Equal, toItem: contentView, attribute: .CenterY, multiplier: 1, constant: 0))
-        scrollView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:[image(==size)]", options: NSLayoutFormatOptions(rawValue: 0), metrics: metrics, views: ["image" : secondHelpImage]))
-        scrollView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[image(==size)]", options: NSLayoutFormatOptions(rawValue: 0), metrics: metrics, views: ["image" : secondHelpImage]))
-        keepView(secondHelpImage, onPage: 1)
-        
-        thirdHelpImage.backgroundColor = .lightGrayColor()
-        contentView.addSubview(thirdHelpImage)
-        contentView.addConstraint(NSLayoutConstraint(item: thirdHelpImage, attribute: .CenterY, relatedBy: .Equal, toItem: contentView, attribute: .CenterY, multiplier: 1, constant: 0))
-        scrollView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:[image(==size)]", options: NSLayoutFormatOptions(rawValue: 0), metrics: metrics, views: ["image" : thirdHelpImage]))
-        scrollView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[image(==size)]", options: NSLayoutFormatOptions(rawValue: 0), metrics: metrics, views: ["image" : thirdHelpImage]))
-        keepView(thirdHelpImage, onPage: 2)
-    }
-    
-    private func configureEventViews() {
-        resultTimeline.translatesAutoresizingMaskIntoConstraints = false
-        firstEventView.translatesAutoresizingMaskIntoConstraints = false
-        contentView.addSubview(firstEventView)
-        contentView.addConstraint(NSLayoutConstraint(item: firstEventView, attribute: .CenterY, relatedBy: .Equal, toItem: resultTimeline, attribute: .CenterY, multiplier: 1, constant: 0))
-        scrollView.addConstraint(NSLayoutConstraint(item: firstEventView, attribute: .CenterX, relatedBy: .Equal, toItem: resultTimeline, attribute: .CenterX, multiplier: 1, constant: -100))
-        keepView(firstEventView, onPages: [0, 1, 2])
-        
-        secondEventView.translatesAutoresizingMaskIntoConstraints = false
-        contentView.addSubview(secondEventView)
-        let secondVerticalConstraint = NSLayoutConstraint(item: secondEventView, attribute: .CenterY, relatedBy: .Equal, toItem: resultTimeline, attribute: .CenterY, multiplier: 1, constant: 0)
-        contentView.addConstraint(secondVerticalConstraint)
-        let secondHorisontalConstraint = NSLayoutConstraint(item: secondEventView, attribute: .CenterX, relatedBy: .Equal, toItem: resultTimeline, attribute: .CenterX, multiplier: 1, constant: 0)
-        scrollView.addConstraint(secondHorisontalConstraint)
-        keepView(secondEventView, onPages: [0, 1, 2])
-        
-        let secondVerticalAnimation = ConstraintConstantAnimation(superview: scrollView, constraint: secondVerticalConstraint)
-        secondVerticalAnimation[0] = 52
-        secondVerticalAnimation[1] = 0
-        animator.addAnimation(secondVerticalAnimation)
-        
-        let secondHorisontalAnimation = ConstraintConstantAnimation(superview: scrollView, constraint: secondHorisontalConstraint)
-        secondHorisontalAnimation[0] = 50
-        secondHorisontalAnimation[1] = 0
-        animator.addAnimation(secondHorisontalAnimation)
-        
-        completedEventView.translatesAutoresizingMaskIntoConstraints = false
-        contentView.addSubview(completedEventView)
-        let completedVerticalConstraint = NSLayoutConstraint(item: completedEventView, attribute: .CenterY, relatedBy: .Equal, toItem: resultTimeline, attribute: .CenterY, multiplier: 1, constant: 0)
-        contentView.addConstraint(completedVerticalConstraint)
-        let completedHorisontalConstraint = NSLayoutConstraint(item: completedEventView, attribute: .CenterX, relatedBy: .Equal, toItem: resultTimeline, attribute: .CenterX, multiplier: 1, constant: 100)
-        scrollView.addConstraint(completedHorisontalConstraint)
-        keepView(completedEventView, onPages: [1, 2])
-        
-        let completedVerticalAnimation = ConstraintConstantAnimation(superview: scrollView, constraint: completedVerticalConstraint)
-        completedVerticalAnimation[1] = 52
-        completedVerticalAnimation[2] = 0
-        animator.addAnimation(completedVerticalAnimation)
-        
-        let completedHorisontalAnimation = ConstraintConstantAnimation(superview: scrollView, constraint: completedHorisontalConstraint)
-        completedHorisontalAnimation[1] = 50
-        completedHorisontalAnimation[2] = 100
-        animator.addAnimation(completedHorisontalAnimation)
-        
-        let completedHideAnimation = HideAnimation(view: completedEventView, showAt: 0.9)
-        animator.addAnimation(completedHideAnimation)
-    }
-    
     override func numberOfPages() -> Int {
         return 5
+    }
+    
+//    MARK: UIInterfaceOrientationMask Portrait only
+    
+    override func shouldAutorotate() -> Bool {
+        return false
+    }
+    
+    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.Portrait
     }
 }
